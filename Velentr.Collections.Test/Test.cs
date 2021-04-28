bea56d8e5a8da40fa0715fef1a4e2549012adf79
Christian Webber <ricky.webber@hotmail.com>
2021-04-28 16:59:40 -0400

    Added initial projects for the dev env and test env

---
+++ Velentr.Collections.Test/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Velentr.Collections.Test
+{
+    class LibraryTest
+    {
+        [Test]
+        public void Test()
+        {
+            //var library = new Library();
+            //Assert.AreEqual(true, library.DoThing());
+        }
+    }
+}
