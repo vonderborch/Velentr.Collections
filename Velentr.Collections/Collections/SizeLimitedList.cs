b1815c772d9f1cbdcd57a45a183240af6c62a53d
Christian Webber <ricky.webber@hotmail.com>
2022-07-26 11:39:05 -0400

    Added SizeLimitedPool collection, minor tweaks/fixes for SizeLimitedList.

--- Velentr.Collections/Collections/SizeLimitedList.cs
+++ Velentr.Collections/Collections/SizeLimitedList.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+
 using Velentr.Collections.CollectionActions;
 using Velentr.Collections.Exceptions;
 
@@ -9,65 +12,74 @@ namespace Velentr.Collections.Collections
     /// <summary>
     ///     A list that is limited in maximum size.
     /// </summary>
-    ///
     /// <typeparam name="T"> Generic type parameter. </typeparam>
-    ///
-    /// <seealso cref="Collection"/>
-    /// <seealso cref="IEnumerable{T}"/>
-    /// <seealso cref="IEnumerable"/>
+    /// <seealso cref="Collection" />
+    /// <seealso cref="IEnumerable{T}" />
+    /// <seealso cref="IEnumerable" />
     [DebuggerDisplay("Count = {Count}, MaxSize = {MaxSize}")]
     public class SizeLimitedList<T> : Collection, IEnumerable<T>, IEnumerable
     {
         /// <summary>
         ///     The list.
         /// </summary>
-        private List<T> _list;
+        private readonly List<T> _list;
 
         /// <summary>
         ///     Constructor.
         /// </summary>
-        ///
         /// <param name="maxSize">        (Optional) The maximum size of the. </param>
         /// <param name="actionWhenFull"> (Optional) The action when full. </param>
         public SizeLimitedList(long maxSize = 32, SizeLimitedFullAction actionWhenFull = SizeLimitedFullAction.PopOldestItem)
         {
-            _list = new List<T>();
-            ActionWhenFull = actionWhenFull;
-            MaxSize = maxSize;
+            if (maxSize < 2)
+            {
+                throw new ArgumentException($"{nameof(maxSize)} must be at least 2!", nameof(maxSize));
+            }
+
+            this._list = new List<T>();
+            this.ActionWhenFull = actionWhenFull;
+            this.MaxSize = maxSize;
         }
 
         /// <summary>
         ///     Constructor.
         /// </summary>
-        ///
         /// <param name="capacity">       The capacity. </param>
         /// <param name="maxSize">        (Optional) The maximum size of the. </param>
         /// <param name="actionWhenFull"> (Optional) The action when full. </param>
         public SizeLimitedList(int capacity, long maxSize = 32, SizeLimitedFullAction actionWhenFull = SizeLimitedFullAction.PopOldestItem)
         {
-            _list = new List<T>(capacity);
-            ActionWhenFull = actionWhenFull;
-            MaxSize = maxSize;
+            if (maxSize < 2)
+            {
+                throw new ArgumentException($"{nameof(maxSize)} must be at least 2!", nameof(maxSize));
+            }
+
+            this._list = new List<T>(capacity);
+            this.ActionWhenFull = actionWhenFull;
+            this.MaxSize = maxSize;
         }
 
         /// <summary>
         ///     Constructor.
         /// </summary>
-        ///
         /// <param name="collection">     The collection. </param>
         /// <param name="maxSize">        (Optional) The maximum size of the. </param>
         /// <param name="actionWhenFull"> (Optional) The action when full. </param>
         public SizeLimitedList(IEnumerable<T> collection, long maxSize = 32, SizeLimitedFullAction actionWhenFull = SizeLimitedFullAction.PopOldestItem)
         {
-            _list = new List<T>(collection);
-            ActionWhenFull = actionWhenFull;
-            MaxSize = maxSize;
+            if (maxSize < 2)
+            {
+                throw new ArgumentException($"{nameof(maxSize)} must be at least 2!", nameof(maxSize));
+            }
+
+            this._list = new List<T>(collection);
+            this.ActionWhenFull = actionWhenFull;
+            this.MaxSize = maxSize;
         }
 
         /// <summary>
         ///     Gets or sets the action when full.
         /// </summary>
-        ///
         /// <value>
         ///     The action when full.
         /// </value>
@@ -76,7 +88,6 @@ namespace Velentr.Collections.Collections
         /// <summary>
         ///     Gets or sets the maximum size.
         /// </summary>
-        ///
         /// <value>
         ///     The maximum size of the.
         /// </value>
@@ -85,140 +96,15 @@ namespace Velentr.Collections.Collections
         /// <summary>
         ///     Indexer to get or set items within this collection using array index syntax.
         /// </summary>
-        ///
         /// <param name="index"> Zero-based index of the entry to access. </param>
-        ///
         /// <returns>
         ///     The indexed item.
         /// </returns>
         public T this[int index]
         {
-            get => _list[index];
-            set => _list[index] = value;
-        }
+            get => this._list[index];
 
-        /// <summary>
-        ///     Adds an item and returns any popped items.
-        /// </summary>
-        ///
-        /// <param name="item"> The item. </param>
-        ///
-        /// <returns>
-        ///    The item that was popped to make space for the new item.
-        /// </returns>
-        public T AddItem(T item)
-        {
-            _list.Add(item);
-
-            if (_list.Count >= MaxSize)
-            {
-                switch (ActionWhenFull)
-                {
-                    case SizeLimitedFullAction.PopOldestItem:
-                        var newItem = _list[0];
-                        _list.RemoveAt(0);
-                        return newItem;
-
-                    case SizeLimitedFullAction.PopNewestItem:
-                        _list.RemoveAt(_list.Count - 1);
-                        return item;
-                }
-            }
-
-            IncrementCount();
-            return default(T);
-        }
-
-        /// <summary>
-        ///     Adds items and returns any popped items.
-        /// </summary>
-        ///
-        /// <param name="items"> The items. </param>
-        ///
-        /// <returns>
-        ///     The items that were popped to make space for the new item.
-        /// </returns>
-        public List<T> AddItem(IEnumerable<T> items)
-        {
-            foreach (var item in items)
-            {
-                _list.Add(item);
-                IncrementCount();
-            }
-
-            var removedItems = new List<T>();
-            while (_list.Count >= MaxSize && ActionWhenFull != SizeLimitedFullAction.Ignore)
-            {
-                switch (ActionWhenFull)
-                {
-                    case SizeLimitedFullAction.PopOldestItem:
-                        removedItems.Add(_list[0]);
-                        _list.RemoveAt(0);
-                        break;
-
-                    case SizeLimitedFullAction.PopNewestItem:
-                        removedItems.Add(_list[_list.Count - 1]);
-                        _list.RemoveAt(_list.Count - 1);
-                        break;
-                }
-
-                DecrementCount();
-            }
-
-            return removedItems;
-        }
-
-        /// <summary>
-        ///     Clears the collection.
-        /// </summary>
-        ///
-        /// <seealso cref="Velentr.Collections.Collections.Collection.Clear()"/>
-        public override void Clear()
-        {
-            _version = 0;
-            var oldCount = Count;
-            _list.Clear();
-            UpdateCount(-oldCount);
-        }
-
-        /// <summary>
-        ///     Performs application-defined tasks associated with freeing, releasing, or resetting
-        ///     unmanaged resources.
-        /// </summary>
-        ///
-        /// <seealso cref="Velentr.Collections.Collections.Collection.Dispose()"/>
-        public override void Dispose()
-        {
-            Clear();
-            _disposed = true;
-        }
-
-        /// <summary>
-        ///     Determine if 'item' exists.
-        /// </summary>
-        ///
-        /// <param name="item"> The item. </param>
-        ///
-        /// <returns>
-        ///     True if it succeeds, false if it fails.
-        /// </returns>
-        public bool Exists(T item)
-        {
-            return _list.FindIndex(x => x.Equals(item)) != -1;
-        }
-
-        /// <summary>
-        ///     Searches for the first index.
-        /// </summary>
-        ///
-        /// <param name="item"> The item. </param>
-        ///
-        /// <returns>
-        ///     The found index.
-        /// </returns>
-        public int FindIndex(T item)
-        {
-            return _list.FindIndex(x => x.Equals(item));
+            set => this._list[index] = value;
         }
 
         /// <summary>
@@ -244,69 +130,196 @@ namespace Velentr.Collections.Collections
         }
 
         /// <summary>
+        ///     Adds an item and returns any popped items.
+        /// </summary>
+        /// <param name="item"> The item. </param>
+        /// <returns>
+        ///     The item that was popped to make space for the new item.
+        /// </returns>
+        public T AddItem(T item)
+        {
+            this._list.Add(item);
+
+            if (this._list.Count >= this.MaxSize)
+            {
+                switch (this.ActionWhenFull)
+                {
+                    case SizeLimitedFullAction.PopOldestItem:
+                        var newItem = this._list[0];
+                        this._list.RemoveAt(0);
+
+                        return newItem;
+
+                    case SizeLimitedFullAction.PopNewestItem:
+                        this._list.RemoveAt(this._list.Count - 2);
+
+                        return item;
+                }
+            }
+
+            IncrementCount();
+
+            return default;
+        }
+
+        /// <summary>
+        ///     Adds items and returns any popped items.
+        /// </summary>
+        /// <param name="items"> The items. </param>
+        /// <returns>
+        ///     The items that were popped to make space for the new item.
+        /// </returns>
+        public List<T> AddItem(IEnumerable<T> items)
+        {
+            var removedItems = new List<T>();
+            var itemsToRemove = -1;
+            var enumerable = items.ToList();
+            while (this._list.Count + enumerable.Count >= this.MaxSize && this.ActionWhenFull != SizeLimitedFullAction.Ignore)
+            {
+                switch (this.ActionWhenFull)
+                {
+                    case SizeLimitedFullAction.PopOldestItem:
+                        removedItems.Add(this._list[0]);
+                        this._list.RemoveAt(0);
+
+                        break;
+
+                    case SizeLimitedFullAction.PopNewestItem:
+                        removedItems.Add(this._list[this._list.Count - 1]);
+                        this._list.RemoveAt(this._list.Count - 1);
+
+                        break;
+                }
+
+                DecrementCount();
+                if (this._list.Count == 0)
+                {
+                    itemsToRemove = enumerable.Count - (int) this.MaxSize;
+
+                    break;
+                }
+            }
+
+            while (itemsToRemove > -1)
+            {
+                enumerable.RemoveAt(0);
+                itemsToRemove--;
+            }
+
+            foreach (var item in enumerable)
+            {
+                this._list.Add(item);
+                IncrementCount();
+            }
+
+            return removedItems;
+        }
+
+        /// <summary>
+        ///     Clears the collection.
+        /// </summary>
+        /// <seealso cref="Velentr.Collections.Collections.Collection.Clear()" />
+        public override void Clear()
+        {
+            this._version = 0;
+            var oldCount = this.Count;
+            this._list.Clear();
+            UpdateCount(-oldCount);
+        }
+
+        /// <summary>
+        ///     Performs application-defined tasks associated with freeing, releasing, or resetting
+        ///     unmanaged resources.
+        /// </summary>
+        /// <seealso cref="Velentr.Collections.Collections.Collection.Dispose()" />
+        public override void Dispose()
+        {
+            Clear();
+            this._disposed = true;
+        }
+
+        /// <summary>
+        ///     Determine if 'item' exists.
+        /// </summary>
+        /// <param name="item"> The item. </param>
+        /// <returns>
+        ///     True if it succeeds, false if it fails.
+        /// </returns>
+        public bool Exists(T item)
+        {
+            return this._list.FindIndex(x => x.Equals(item)) != -1;
+        }
+
+        /// <summary>
+        ///     Searches for the first index.
+        /// </summary>
+        /// <param name="item"> The item. </param>
+        /// <returns>
+        ///     The found index.
+        /// </returns>
+        public int FindIndex(T item)
+        {
+            return this._list.FindIndex(x => x.Equals(item));
+        }
+
+        /// <summary>
         ///     Gets an item.
         /// </summary>
-        ///
         /// <param name="index"> Zero-based index of the. </param>
-        ///
         /// <returns>
         ///     The item.
         /// </returns>
         public T GetItem(int index)
         {
-            if (index < 0 || index >= _list.Count)
+            if (index < 0 || index >= this._list.Count)
             {
-                return default(T);
+                return default;
             }
 
-            return _list[index];
+            return this._list[index];
         }
 
         /// <summary>
         ///     Gets the snapshot.
         /// </summary>
-        ///
         /// <returns>
         ///     The snapshot.
         /// </returns>
         public List<T> GetSnapshot()
         {
-            return new List<T>(_list);
+            return new List<T>(this._list);
         }
 
         /// <summary>
         ///     Removes at described by index.
         /// </summary>
-        ///
         /// <param name="index"> Zero-based index of the. </param>
-        ///
         /// <returns>
         ///     True if it succeeds, false if it fails.
         /// </returns>
         public bool RemoveAt(int index)
         {
-            if (index < 0 || index >= Count)
+            if (index < 0 || index >= this.Count)
             {
                 return false;
             }
 
-            _list.RemoveAt(index);
+            this._list.RemoveAt(index);
             DecrementCount();
+
             return true;
         }
 
         /// <summary>
         ///     Removes the item described by item.
         /// </summary>
-        ///
         /// <param name="item"> The item. </param>
-        ///
         /// <returns>
         ///     True if it succeeds, false if it fails.
         /// </returns>
         public bool RemoveItem(T item)
         {
-            var output = _list.Remove(item);
+            var output = this._list.Remove(item);
 
             if (output)
             {
@@ -319,9 +332,7 @@ namespace Velentr.Collections.Collections
         /// <summary>
         ///     Removes the item described by item.
         /// </summary>
-        ///
         /// <param name="items"> The items. </param>
-        ///
         /// <returns>
         ///     True if it succeeds, false if it fails.
         /// </returns>
@@ -354,15 +365,15 @@ namespace Velentr.Collections.Collections
         /// <exception cref="CollectionModifiedException"></exception>
         private IEnumerator<T> InternalGetEnumerator()
         {
-            var enumeratorVersion = _version;
-            for (var i = 0; i < Count; i++)
+            var enumeratorVersion = this._version;
+            for (var i = 0; i < this.Count; i++)
             {
-                if (enumeratorVersion != _version)
+                if (enumeratorVersion != this._version)
                 {
                     throw new CollectionModifiedException();
                 }
 
-                yield return _list[i];
+                yield return this._list[i];
             }
         }
     }
