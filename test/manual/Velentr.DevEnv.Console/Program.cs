4991e833861aafb88c2aa7672742f81fe787e7fc
Christian Webber <ricky.webber@hotmail.com>
2022-11-18 10:48:30 -0500

    Added test game app

---
+++ test/manual/Velentr.DevEnv.Console/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Velentr.DevEnv.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // insert test code here
+        }
+    }
+}
